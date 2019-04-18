2766c16841dea09c241ff438489db0e1ccd76be1
Aluno <aluno@eteclencois.com.br>
2019-04-18 12:22:06 -0300

    aula 18/04

--- src/app/categorias/categorias-pesquisa/categorias-pesquisa.component.ts
+++ src/app/categorias/categorias-pesquisa/categorias-pesquisa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaFiltro, CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categorias-pesquisa',
@@ -7,16 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasPesquisaComponent implements OnInit {
 
-  categorias = [
-    {id: '1', nome: 'Informática'},
-    {id: '2', nome: "Escritório"},
-    {id: '3', nome: 'Perfumaria'},
-    {id: '4', nome: 'Lazer'}
-  ]
+  totalRegistros = 0;
+  filtro = new CategoriaFiltro();
+  categorias = []
   
-  constructor() { }
+  constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit() {
+    this.pesquisar();
+  }
+
+  pesquisar(pagina = 0){
+    this.filtro.pagina = pagina;
+
+    this.categoriaService.pesquisar(this.filtro).then(resultado => {
+      this.totalRegistros = resultado.total;
+      this.categorias = resultado.categorias;
+    })
+
   }
 
 }
